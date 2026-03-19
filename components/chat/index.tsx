4ef2ff70ef3747cd8c3dd2fecaf9347a596367a8
sanson-pedro <sanson.pedroh@gmail.com>
2026-03-19 15:46:05 -0300

    refactor: extract chat and code-editor into components

---
+++ components/chat/index.tsx
@@ -0,0 +1,11 @@
+export default function Chat() {
+  return (
+    <section className="h-full flex flex-col">
+      <header>
+        <h1>Header</h1>
+      </header>
+      <div className="flex-1">mensagem</div>
+      <div>parte da pergunta do chat</div>
+    </section>
+  );
+}
