fd10956aae2114884738b480551ff6a3fe6d9296
sanson-pedro <sanson.pedroh@gmail.com>
2026-03-19 15:45:46 -0300

    refactor: extract chat and code-editor into components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
+import Chat from "@/components/chat"
+import CodeEditor from "@/components/code-editor";
+
 export default function Home() {
   return (
     <div className="h-screen grid grid-cols-3">
       <div className="col-span-2 bg-[#1A1A1A] border-r border-[#262626]">
-        editor
+        <CodeEditor/>
       </div>
       <div className="col-span-1 bg-[#1A1A1A]">
-        <section className="h-full flex flex-col">
-          <header>Chat part</header>
-          <div className="flex-1">mensagem</div>
-          <div>parte da pergunta do chat</div>
-        </section>
+        <Chat/>
       </div>
     </div>
   );
